5bfdcd6b528625be32bca5b6fb8d561f8323615e
wasimasaad <wasimasaad14@gmail.com>
2025-01-04 11:38:12 +0300

    Fix binding to 0.0.0.0 for Render deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from genetic_algorithm import TransportOptimizer
+import os
 
 app = Flask(__name__)
 
@@ -43,7 +44,10 @@ def index():
 
     return render_template('index.html')
 
-if __name__ == '__main__':    
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+if __name__ == '__main__': 
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port, debug=True)
+
+   
 
     
