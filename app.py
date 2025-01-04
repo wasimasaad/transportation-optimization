9d615ec4aad07faa4a977fbec575484836bbe717
wasimasaad <wasimasaad14@gmail.com>
2025-01-04 11:06:11 +0300

    Fix binding to 0.0.0.0 for Render deployment

--- app.py
+++ app.py
@@ -43,5 +43,7 @@ def index():
 
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__':    
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
+    
